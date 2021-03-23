dd59a3aa4a24b0646ba104595b81e16d1f41226f
Lucas Beley <lucas.beley@hotmail.fr>
2021-03-23 17:08:10 +0100

    Test with empty DB for call_the_monster

--- tests/Controller/SecondStepControllerTest.php
+++ tests/Controller/SecondStepControllerTest.php
@@ -15,12 +15,15 @@ class SecondStepControllerTest extends ControllerTestCase
 	/**
 	 * Test the call of the monster that should be OK
 	 *
-	 * TODO Test if DB is empty
+	 * @dataProvider provideLoadFixtures
+	 * @param bool $loadFixtures
 	 */
-	public function testCallTheMonster()
+	public function testCallTheMonster(bool $loadFixtures)
 	{
 		$serializer = new Serializer([new GetSetMethodNormalizer()], [new JsonEncoder()]);
-		$this->loadFixtures([FriendsFixture::class], false, self::DEFAULT_DOC_MANAGER_SERVICE);
+		if ($loadFixtures) {
+			$this->loadFixtures([FriendsFixture::class], false, self::DEFAULT_DOC_MANAGER_SERVICE);
+		}
 		$repo = $this->documentManager->getRepository(Friend::class);
 		$originalSizeDb = count($repo->findAll());
 
@@ -31,14 +34,24 @@ class SecondStepControllerTest extends ControllerTestCase
 		$this->assertEquals(200, self::$client->getResponse()->getStatusCode());
 
 		//Returned object can be unserialized in a Friend document
-		try {
-			/** @var Friend $responseContent */
-			$responseContent = $serializer->deserialize(self::$client->getResponse()->getContent(), Friend::class, 'json');
-			$this->assertInstanceOf(Friend::class, $responseContent);
-		} catch (Exception $exception) {
-			$this->fail("Unserialization of json to Friend document failed.");
+		if ($loadFixtures) {
+			try {
+				/** @var Friend $responseContent */
+				$responseContent = $serializer->deserialize(self::$client->getResponse()->getContent(), Friend::class, 'json');
+				$this->assertInstanceOf(Friend::class, $responseContent);
+			} catch (Exception $exception) {
+				$this->fail("Unserialization of json to Friend document failed.");
+			}
 		}
 
-		$this->assertCount($originalSizeDb - 1, $repo->findAll());
+		$this->assertCount($originalSizeDb - $loadFixtures, $repo->findAll());
+	}
+
+	public function provideLoadFixtures(): array
+	{
+		return [
+			true,
+			false
+		];
 	}
 }
